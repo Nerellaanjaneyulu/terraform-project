3a072654e7a60a6877e1104de8bc045f7252d7fc
Anjaneyulu <127377390+Nerellaanjaneyulu@users.noreply.github.com>
2023-07-11 12:42:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 #this file consists of code for instances and sg
 provider "aws" {
 region = "us-east-1"
-access_key = ""
-secret_key = ""
+access_key = "AKIAVBV7JNK6UKC76IPO"
+secret_key = "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"
 }
 
 resource "aws_instance" "one" {
